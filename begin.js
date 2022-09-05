c8fd44f75c586f9f43298e8ceada418af3f55a32
Rodrigo Siqueira <rodrigo.dsiqueira1@gmail.com>
2022-09-05 12:16:09 -0300

    creating some js dom structures

--- begin.js
+++ begin.js
@@ -1,5 +1,29 @@
-var x = 10;
-var y = 5;
-var z;
-z = x + y;
-console.log(`O valor da soma de ${x} e ${y} é ${z}`)
+var current_number = 0;
+var current_number_wrapper = document.getElementById("current-number");
+
+function increment() {
+    current_number = current_number + 1;
+    current_number_wrapper.innerHTML = current_number;
+}
+
+function decrement() {
+    current_number = current_number - 1;
+    current_number_wrapper.innerHTML = current_number;
+
+}
+
+function change_color() {
+    if (current_number_wrapper.innerHTML >= 0) {
+        current_number_wrapper.style.color('green');
+    } else {
+        current_number_wrapper.style.color('red');
+    }
+}
+
+
+
+
+
+
+
+
